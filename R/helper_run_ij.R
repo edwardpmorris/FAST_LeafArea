2b43ab162c4774d6a1dcb7334e331cdc57e0ac43
edwardpmorris <edward.morris@uca.es>
2016-01-21 22:40:06 +0100

    made into package

--- R/helper_run_ij.R
+++ R/helper_run_ij.R
@@ -1,11 +1,30 @@
 #' Helper function to set up LeafArea run.ij
 #'
-#' As long as ImageJ application is installed in the follwoing directory, you do not have to specify the path to ImageJ
-#' /Applications/ImageJ/ <Mac>
-#' C:\Program Files\ImageJ\ <Windows>
-#' Linux always needs to specify the path to ImageJ. For example, path = "~/ImageJ/"
+#' @description  A simple wrapper passing project specific setings to \pckg{LeafArea} and ImageJ. 
+#' 
+#' @details If \link[http://rsb.info.nih.gov/ij/]{ImageJ} is installed in one of the default directories, you do not have to specify the path to ImageJ.
 #'
+#'  \code{/Applications/ImageJ/ <Mac>}
+#'  
+#'  \code{C:\\Program Files\\ImageJ\\ <Windows>}
+#' 
+#' If the OS is Linux you do need to specify the path to ImageJ. 
+#' 
+#' For example, \code{path = "~/ImageJ/"}
 #'
+#' The number of pixels per a known distance should be supplied. 
+#' For an A4 sheet scanned at 150 dpi; 58.5 px/cm
+#' Use the option \code{check.image=T} to view the auto-thresholding and particle analyis results in the GUI of ImageJ.
+#' 
+#' The amount of 'cropping' of the edges of the image is set using \code{trim.pixel}; a value of 250 is suitable for the FAST template. Probelmatic images can be manually cropped without changing the resolution, in which case trim should be adjusted accordingly.
+#' 
+#' Known bugs: 
+#' 
+#' Sometimes the analysis gives errors if repeated in same session. Solution is to save results and start a new R session before re-running. 
+#' 
+#' Some images cause errors. Check the resolution and dimensions of the images.
+#'       
+#' 
 #' @param set.directory Set directory that contains leaf images. 
 #' @param trim.pixel Number of pixels removed from edges in the analysis.
 #' @param check.image Do you want imageJ to open and show you the analysis of each image?
@@ -17,21 +36,34 @@
 #' @export
 #'
 #' @examples
-#' # if using Linux define the path to the ImageJ executable, if not, comment out.
-#' # recommend download and extract the standalone version, rather than use a package manager.
+#' # If using Linux define the path to the ImageJ executable,
+#' # if not, comment out; Leaf Area should find it.
+#' # Recommend downloading and extracting the standalone version,
+#' # rather than using a package manager.
+#' 
+#' # set path (Linux)
 #' path.imagej <- "~/ImageJ"
-#' #path.image <- file.path(system.file("images", package="FAST_LeafArea"),"")
-#' path.image <- "inst/images"
-#' # for A4 paper 300 dpi; 21 cm = 1238 px 
-#' area <- helper.leafarea(set.directory=path.image, path.imagej=path.imagej, trim.pixel=250, distance.pixel=1238, known.distance=21, check.image=F)
+#' 
+#' # set path to test images directory
+#' # A4 scans at 150 dpi, 1 cm = 58.5 px
+#' path.image <- file.path(system.file("images"
+#'                        , package="FASTLeafArea"),"")
+#' 
+#' # run analysis on images using ImageJ 
+#' area <- helper.leafarea(set.directory=path.image, path.imagej=path.imagej
+#'                        , trim.pixel=250, distance.pixel=58.5, known.distance=1
+#'                        , check.image=F)
+#' 
 #' # write to file in working directory
-#' write.csv(area, paste0(paste(attr(area,"standard.name"), attr(area,"units"), sep="_", ".csv")), quote = T, na = "NA", row.names = F)
+#' write.csv(area, paste0(paste(attr(area,"standard.name")
+#'          , attr(area,"units"), sep="_", ".csv"))
+#'          , quote = T, na = "NA", row.names = F)
 #' 
 helper.leafarea <- function (set.directory, trim.pixel=0, check.image=F, distance.pixel=1, known.distance=1, path.imagej=NULL) {
   # check LeafArea package is available
-  require(LeafArea)
+  #require(LeafArea)
   # run imageJ using "standard FAST project settings"
-  out <- run.ij(
+  out <- LeafArea::run.ij(
       path.imagej = path.imagej
     , set.directory = set.directory
     , trim.pixel = trim.pixel
@@ -54,4 +86,3 @@ helper.leafarea <- function (set.directory, trim.pixel=0, check.image=F, distanc
   attr(out,"units") <- "m2"
   return(out)
 }
-
